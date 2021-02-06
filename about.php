26b16e91cbf81d456eecc220eaa40b67b3217e56
AnmolPrajapat <76764486+AnmolPrajapat@users.noreply.github.com>
2021-02-06 12:54:00 +0530

    styling of menu can further use

--- about.php
+++ about.php
@@ -6,6 +6,19 @@
 <body bgcolor="lightgreen">
 	<h1> Here you can see our menu <h1>
 		<a href="myimage.php">click here to see Menu</a>
+		<div class="menu1">
+		<label><a href="myimage.php">see dishes</label>
+			<hr><br>
+			
+				<ol>
+
+		       <li><a href="images/fastfood.jpg" >fastfood corner</a></li>
+		
+		       <li><a href="bar.jpg">drinks</a>
+		       </li>
+		  </ol>
+	
+	</div>
 		
 
 </body>
